79e7f71a321e1b4faf9ac617f0abb3b121395dc1
Anton Ras <antonras@GMAIL.COM>
2024-07-07 09:17:23 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:first_app/gradient_container.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: Scaffold(
+      body: GradientContainer(
+        Color.fromARGB(255, 221, 129, 24),
+        Color.fromRGBO(243, 222, 33, 1),
+      ),
+    ),
+  ));
+}
