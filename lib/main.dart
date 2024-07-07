4526facdb2fc7bea863d9918d8c944b5358785f4
Anton Ras <antonras@GMAIL.COM>
2024-07-07 19:26:05 +0200

    chore: Refactor code to use DiceRoller component in GradientContainer

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:first_app/gradient_container.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
+  runApp( const MaterialApp(
     home: Scaffold(
       body: GradientContainer(
         Color.fromARGB(255, 221, 129, 24),
